d0a47a31ef71333c5143a9124197e6ba5f536c74
pablombatalla <42776186+pablombatalla@users.noreply.github.com>
2021-08-12 17:25:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 $name = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['name']);
 
 $ch = curl_init();
-// IMPORTANT: the below line is a security risk, read https://paragonie.com/blog/2017/10/certainty-automated-cacert-pem-management-for-php-software
-// in most cases, you should set it to true
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, 'http://api-lamp-service.lamp.svc.cluster.local./?name=' . $name);
